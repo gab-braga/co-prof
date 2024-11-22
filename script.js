52c78ea1ed97cebad4163d5fc93a45c0fdf2a987
gab-braga <gabrielbraga0712@gmail.com>
2024-11-22 12:04:28 -0300

    feat: openai api with whisper

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 let mediaRecorder;
 let audiosChunks = [];
 
+async function transcriber(audio) {
+    const formData = new FormData();
+    formData.append("file", audio);
+    formData.append("model", "whisper-1");
+    formData.append("language", "pt");
+    formData.append("response_format", "json");
+    formData.append("temperature", "0");
+
+    const response = await fetch("https://api.openai.com/v1/audio/transcriptions", {
+        method: "POST",
+        body: formData,
+        headers: {
+            Authorization: "Bearer ..."
+        }
+    });
+
+    return response.json();
+}
+
+
 async function startRecording() {
     try {
         const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
@@ -14,12 +34,19 @@ async function startRecording() {
             console.log("Gravação iniciada.");
         });
 
-        mediaRecorder.onstop = (_ => {
+        mediaRecorder.onstop = (async () => {
             console.log("Gravação finalizada.");
             const audioBlob = new Blob(audiosChunks, { type: "audio/wav" });
             const audioURL = URL.createObjectURL(audioBlob);
             const audio = new Audio(audioURL);
+            
+            document.write("Transcrevendo...");
             audio.play();
+
+            const transcription = await transcriber(audioBlob);
+            console.log(transcription);
+            document.write(transcription.text)
+            
             audiosChunks = [];
         });
 
