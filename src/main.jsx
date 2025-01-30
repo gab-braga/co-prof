86ef6b9f17b4d8d48be070b5c3414d5defd589ed
gab-braga <gabrielbraga0712@gmail.com>
2025-01-29 21:54:50 -0300

    feat: fonts and custom style added

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
 import './index.css';
 import './css/bootstrap.css';
-import App from './App.jsx';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
