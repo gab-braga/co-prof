23bd2c674461f539e7172b8c6593e77629d94943
gab-braga <gabrielbraga0712@gmail.com>
2025-04-09 11:27:51 -0300

    fix: regex email fixed

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -1,3 +1,5 @@
-const EMAIL_REGEX = /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gm;
+// Made by regexr.com
+const EMAIL_REGEX =
+  /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
 
 export { EMAIL_REGEX };
