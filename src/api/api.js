f0eba02283f2e4a4fe9302336a64bb3b66b53341
gab-braga <gabrielbraga0712@gmail.com>
2025-05-20 14:03:25 -0300

    fix: base url api updated

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // const baseURL = 'http://localhost:8080';
-const baseURL = 'https://coprof.azurewebsites.net';
+// const baseURL = 'https://coprof.azurewebsites.net';
+const baseURL = 'https://co-prof-api-509264289385.us-west1.run.app';
 
 const api = axios.create({
   baseURL,
